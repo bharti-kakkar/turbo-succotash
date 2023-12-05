b68074e63453f95373ca1451fe17ef51c7f448a7
bharti-kakkar <152719766+bharti-kakkar@users.noreply.github.com>
2023-12-05 10:57:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three@110.0.1';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
